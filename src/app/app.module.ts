862c9861ebfc5835106964fb70eaa4179b091607
Charissa Wersen <charissa.wersen@gmail.com>
2021-01-12 17:03:29 -0500

    adding changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { TodoComponent } from './todo/todo.component';
 
+import {FormsModule} from '@angular/forms';
+
 @NgModule({
   declarations: [
     AppComponent,
     TodoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,17 +1,20 @@
+import { stringify } from '@angular/compiler/src/util';
 import { Component, OnInit } from '@angular/core';
+
 interface Todo {
-  task: string,
-  completed: boolean,
+  task: string;
+  completed: boolean;
 }
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.css']
+  styleUrls: ['./todo.component.css'],
 })
-export class TodoComponent implements OnInit {
-  public todos: Todo[];
 
+export class TodoComponent implements OnInit {
+  filter: string = '';
+  todos: Todo[];
   constructor() {
     this.todos = [
       {
@@ -37,11 +40,34 @@ export class TodoComponent implements OnInit {
       {
         task: 'be awesome',
         completed: false,
-      }
-    ]
+      },
+    ];
+  }
+  ngOninit(): void {
+
   }
 
-  ngOnInit(): void {
+  newTask : TodoComponent;
+  addTask (newTask): void {
+    this.todos.push({ task: newTask, completed: false });
   }
 
+  removeTask (i) : void {
+    this.todos.splice(i, 1);
+    if (this.todos.length === 0) {
+      alert("Yay! Your tasks are done!");
+    }
+  }
+
+  completeTask(i) : void {
+     this.todos[i].completed = true;
+  }
+
+  getFilteredResults(): Todo[] {
+    return this.todos.filter((todo) => {
+      return todo.task.toLowerCase().includes(this.filter.toLowerCase());
+    });
+  }
+
+  ngOnInit() {}
 }
